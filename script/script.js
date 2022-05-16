3f9753214603bc5ea9915888f7166f85c0522162
melodoc <liubov_rz@yahoo.com>
2022-05-16 21:22:42 +0400

    fix: small refactoring

--- script/script.js
+++ script/script.js
@@ -2,16 +2,19 @@
 
 import { handleTitleContent } from './handleTitleContent/handleTitleContent.js';
 import { handleInput } from './handleInput/handleInput.js';
+import { appendInputFromTemplate } from './handleInput/appendInputFromTemplate.js';
 import { enableValidation } from './validation/enableValidation.js';
 import { onLoadInputValidations, additionalInputValidations } from './constants/constants.js';
 
 const dashboardTitle = document.querySelector('.dashboard__title');
-handleTitleContent(dashboardTitle);
-
 const addSideIncomeButton = document.querySelectorAll('.header__button')[0];
 const addMandatoryExpensesButton = document.querySelectorAll('.header__button')[1];
 const sideIncomeContainer = addSideIncomeButton.closest('.input-list');
 const mandatoryExpensesContainer = addMandatoryExpensesButton.closest('.input-list');
+
+handleTitleContent(dashboardTitle);
+appendInputFromTemplate('#side-income', sideIncomeContainer);
+appendInputFromTemplate('#mandatory-expenses', mandatoryExpensesContainer);
 enableValidation(onLoadInputValidations);
 
 addSideIncomeButton.addEventListener('click', () => {
@@ -23,11 +26,3 @@ addMandatoryExpensesButton.addEventListener('click', () => {
     handleInput('#mandatory-expenses', mandatoryExpensesContainer);
     enableValidation(additionalInputValidations);
 });
-
-const render = (container, template, place = `beforeend`) => {
-    container.insertAdjacentHTML(place, template);
-};
-
-const insertAfter = (newNode, referenceNode) => {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-};
