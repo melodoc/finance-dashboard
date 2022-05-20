6a98ba3b32c76cc35f1d30e13d37afdfb8225fc3
melodoc <liubov_rz@yahoo.com>
2022-05-21 00:02:58 +0400

    feat: add handleCheckbox functions

--- script/script.js
+++ script/script.js
@@ -5,6 +5,14 @@ import { appendInput } from './handleInput/handleInput.js';
 import { enableValidation } from './validation/enableValidation.js';
 import { validationProps } from './constants/constants.js';
 import { setIncomeProps } from './handleInput/setInputProps.js';
+import {
+    showTermDepositInput,
+    setTermDepositInputRequired,
+    handleAdditionalSelect,
+    hideTermDepositInput,
+    removeRequiredAttributes
+} from './handleCheckbox/handleCheckbox.js';
+
 const dashboardTitle = document.querySelector('.dashboard__title');
 
 const addSideIncomeButton = document.querySelectorAll('.header__button')[0];
@@ -14,6 +22,9 @@ const sideIncomeContainer = addSideIncomeButton.closest('.input-list');
 const addMandatoryExpensesButton = document.querySelectorAll('.header__button')[1];
 const mandatoryExpensesTemplate = document.querySelector('#mandatory-expenses').content;
 const mandatoryExpensesContainer = addMandatoryExpensesButton.closest('.input-list');
+
+const termDepositCheckBox = document.querySelector('#custom-checkbox__input');
+const depositBankSelect = document.querySelector('#deposit-bank');
 
 const sideIncomeProps = setIncomeProps(sideIncomeTemplate, sideIncomeContainer, addSideIncomeButton);
 const mandatoryExpensesProps = setIncomeProps(
@@ -35,4 +46,19 @@ addSideIncomeButton.addEventListener('click', () => {
 
 addMandatoryExpensesButton.addEventListener('click', () => {
     appendInput(mandatoryExpensesProps, true);
+});
+
+termDepositCheckBox.addEventListener('click', () => {
+    if (termDepositCheckBox.checked) {
+        showTermDepositInput();
+        setTermDepositInputRequired();
+        handleAdditionalSelect();
+    } else {
+        hideTermDepositInput();
+        removeRequiredAttributes();
+    }
+});
+
+depositBankSelect.addEventListener('change', () => {
+    handleAdditionalSelect();
 });
