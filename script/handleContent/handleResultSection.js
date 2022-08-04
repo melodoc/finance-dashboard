0dd2b4a200c785b1100d86bb83f660767495d375
melodoc <liubov_rz@yahoo.com>
2022-08-04 17:28:00 +0400

    hotfix: Add getElements to handleResultSection

--- script/handleContent/handleResultSection.js
+++ script/handleContent/handleResultSection.js
@@ -1,21 +1,40 @@
+const getElements = (containerElement, selectors) => {
+    const elements = {};
+    Object.keys(selectors).map((key) => {
+        elements[key] = containerElement.querySelector(selectors[key]);
+    });
+    return elements;
+};
+
 export const handleResultSection = (resultData) => {
     const result = document.querySelector('#result');
-    const budgetMonthResult = result.querySelector('#budget_month-result');
-    const expensesMonthResult = result.querySelector('#expenses_month-result');
-    const budgetDayResult = result.querySelector('#budget_day-result');
-    const incomePeriodResult = result.querySelector('#income_period-result');
-    const bankDepositResult = result.querySelector('#bank-deposit-result');
-    const targetMonthResult = result.querySelector('#target_month-result');
 
-    const summaryIncomeSection = result.querySelector('.additional_income-value');
-    const summaryExpensesSection = result.querySelector('.additional_expenses-value');
+    const {
+        budget,
+        expenses,
+        budgetDay,
+        incomePeriod,
+        bankDeposit,
+        targetMonth,
+        additionalIncome,
+        additionalExpenses
+    } = getElements(result, {
+        budget: '#budget_month-result',
+        expenses: '#expenses_month-result',
+        budgetDay: '#budget_day-result',
+        incomePeriod: '#income_period-result',
+        bankDeposit: '#bank-deposit-result',
+        targetMonth: '#target_month-result',
+        additionalIncome: '.additional_income-value',
+        additionalExpenses: '.additional_expenses-value'
+    });
 
-    budgetMonthResult.value = resultData.total.income;
-    expensesMonthResult.value = resultData.total.expense;
-    budgetDayResult.value = resultData.total.dailyBudget;
-    incomePeriodResult.value = resultData.total.totalSavings;
-    bankDepositResult.value = resultData.total.bankDeposit ? resultData.total.bankDeposit : 'Nothing to count';
-    targetMonthResult.value = resultData.total.goalReachingPeriod;
-    summaryIncomeSection.textContent = resultData.summary.income;
-    summaryExpensesSection.textContent = resultData.summary.expenses;
+    budget.value = resultData.total.income;
+    expenses.value = resultData.total.expense;
+    budgetDay.value = resultData.total.dailyBudget;
+    incomePeriod.value = resultData.total.totalSavings;
+    bankDeposit.value = resultData.total.bankDeposit ? resultData.total.bankDeposit : 'Nothing to count';
+    targetMonth.value = resultData.total.goalReachingPeriod;
+    additionalIncome.textContent = resultData.summary.income;
+    additionalExpenses.textContent = resultData.summary.expenses;
 };
